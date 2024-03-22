256a128cfa4c7c0e7cc4c78661730d0770b30cc6
Pratibha Anand <94226733+Anandprati@users.noreply.github.com>
2024-03-22 13:09:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        color: 'var(--color)',
+        background: 'var(--background)',
+        white: 'var(--white)',
+        black: 'var(--black)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+      },
+    },
+  },
+  plugins: [],
+}
